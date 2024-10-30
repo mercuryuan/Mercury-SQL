77400a493f792f71d7c9877bf4c45d7671f7cde9
Mercuryuan <123456@my.com>
2024-10-30 12:26:15 +0800

    fix C3.sh

--- C3.sh
+++ C3.sh
@@ -26,7 +26,7 @@ echo "train数据库模式已保存至：$PROJECT_ROOT/Data/C3-data/train_schema
 python "$PROJECT_ROOT/Dataset_Tools/C3-style/generate_C3_schema.py" \
     --mode "test" \
     --table_path "$PROJECT_ROOT/Datasets/spider/tables.json" \
-    --input_dataset_path "$PROJECT_ROOT/Datasets/spider/train_dev.json" \
+    --input_dataset_path "$PROJECT_ROOT/Datasets/spider/dev.json" \
     --output_dataset_path "$PROJECT_ROOT/Data/C3-data/dev_schema_C3form.json" \
     --db_path "$PROJECT_ROOT/Datasets/spider/database" \
     --target_type "sql"
