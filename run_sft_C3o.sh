7d3af31edcfac18f02211d38ac712db65bf4dea4
Mercuryuan <123456@my.com>
2024-11-04 20:50:41 +0800

    加入微调数据集(92%)

---
+++ run_sft_C3o.sh
@@ -0,0 +1,6 @@
+cd LLaMA-Factory
+export CUDA_VISIBLE_DEVICES=0  # 指定只用 GPU 0ni
+
+# 使用指定的配置文件启动训练，并重定义 output_dir
+llamafactory-cli train config/sft_Llama3B/C3_orig_train_1_hop_lr_5.yaml
+llamafactory-cli train config/sft_Llama3B/C3_orig_train_1_hop_lr_55.yaml
