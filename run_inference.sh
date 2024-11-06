9c0a826e23b2698d6ab7d17befc732dc9c6a0764
Mercuryuan <123456@my.com>
2024-11-06 15:44:01 +0800

    对比两种模板: add 10个推理任务的配置文件、动态执行10个推理任务的sh脚本

--- run_inference.sh
+++ run_inference.sh
@@ -10,22 +10,27 @@ MODEL_NAME="llama3B"
 if [ ! -d "Results/prediction/$MODEL_NAME" ]; then
     mkdir -p "Results/prediction/$MODEL_NAME"
     echo "文件夹 'Results/prediction/$MODEL_NAME' 已创建。"
-else
-    echo "文件夹 'Results/prediction/$MODEL_NAME' 已存在。"
 fi
 
 # 定义训练命令和对应的日志文件名称
 commands=(
-    "llamafactory-cli train config/inference_Llama3B/predict_C3o.yaml"
-#    "llamafactory-cli train config/inference_Llama3B/predict_C3o_lr_55.yaml"
-#    "llamafactory-cli train config/inference_Llama3B/llama3.yaml"
-#    "llamafactory-cli train config/inference_Llama3B/default.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_default/predict_C3o.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_default/predict_C3o_lr_7.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_default/predict_C3.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_default/predict_DTS_schema.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_default/predict_DTS_sql_generate.yaml"
+#
+    "llamafactory-cli train config/inference_Llama3B/tp_llama3/predict_C3o.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_llama3/predict_C3o_lr_7.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_llama3/predict_C3.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_llama3/predict_DTS_schema.yaml"
+    "llamafactory-cli train config/inference_Llama3B/tp_llama3/predict_DTS_sql_generate.yaml"
 )
 
 
 # 定义可用的 GPU
-gpu_count=2  # 可用 GPU 数量
-gpu_assignments=(0 1)  # GPU 0 和 GPU 1
+gpu_count=4  # 可用 GPU 数量
+gpu_assignments=(0 1 2 3)  # GPU 0 和 GPU 1
 
 # 创建一个空数组来保存当前正在执行的任务的 PID
 declare -a running_jobs
@@ -42,7 +47,6 @@ for i in "${!commands[@]}"; do
         unset running_jobs[$gpu_id]
     done
 
-    # 使用 tee 命令同时输出到终端和日志文件
     CUDA_VISIBLE_DEVICES=$gpu_id eval "${commands[$i]}" &
 
     # 将当前任务的进程 ID 记录到数组中
@@ -51,4 +55,4 @@ done
 
 # 等待所有后台任务完成
 wait
-echo "所有训练任务已完成。"
+echo "所有推理任务已完成。"
